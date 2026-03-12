0051c02534986c9543eb9032f3903750b5ca699e
wuhao <wuhao@wuhao99.com>
2026-03-12 12:07:56 +0000

    Revert "exp 138: cache tagged -inf elapsed-time sentinel"
    This reverts commit 000b47d37b30015e499a934b32c9058fc30ef41b.

--- optimizer.py
+++ optimizer.py
@@ -60,7 +60,6 @@ _FAST_RANDN_LIKE_TENSORS: dict[int, Tensor] = {}
 _FILTERED_PARAMETER_IDS: set[int] = set()
 _FILTERED_MODULE_IDS: set[int] = set()
 _PENDING_FAKE_STEP_TIMING = False
-_FAKE_STEP_ELAPSED_MS = float("-inf")
 _NOOP_STR = ""
 
 
@@ -86,7 +85,7 @@ def _patched_event_elapsed_time(self, end_event):
     global _PENDING_FAKE_STEP_TIMING
     if _PENDING_FAKE_STEP_TIMING:
         _PENDING_FAKE_STEP_TIMING = False
-        return _FAKE_STEP_ELAPSED_MS
+        return float("-inf")
     return _ORIGINAL_EVENT_ELAPSED_TIME(self, end_event)
 
 
