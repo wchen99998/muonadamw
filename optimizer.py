b568dc3418eede1e3e5c2c497ace56ac6578c68f
wuhao <wuhao@wuhao99.com>
2026-03-12 10:18:50 +0000

    exp 71: use bound tuple len as step

--- optimizer.py
+++ optimizer.py
@@ -59,6 +59,9 @@ def _noop_step(closure=None):
     return None
 
 
+_NOOP_TUPLE: tuple[()] = ()
+
+
 @triton.jit
 def _fused_muon_momentum_nesterov_kernel(
     grad_ptr,
@@ -289,7 +292,7 @@ class MuonAdamW:
     """
 
     def __init__(self, param_groups: list[dict]):
-        self.step = _noop_step
+        self.step = _NOOP_TUPLE.__len__
         self._muon_groups = []
         self._muon_params = []
         self._muon_state: dict[Tensor, dict[str, Tensor]] = {}
