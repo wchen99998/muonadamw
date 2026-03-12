1c0aba9a2b988d901528e0833ba98b598e48117e
wuhao <wuhao@wuhao99.com>
2026-03-12 11:09:37 +0000

    Revert "exp 102: return shared empty tuple from empty parameters callable"
    This reverts commit 834b1c34d0654d284a73c1a79591c820b72a3aa9.

--- optimizer.py
+++ optimizer.py
@@ -58,7 +58,6 @@ _ORIGINAL_MODULE_PARAMETERS = torch.nn.Module.parameters
 _FAST_RANDN_LIKE_TENSORS: dict[int, Tensor] = {}
 _FILTERED_PARAMETER_IDS: set[int] = set()
 _FILTERED_MODULE_IDS: set[int] = set()
-_EMPTY_TUPLE = ()
 _NOOP_STR = ""
 
 
@@ -75,7 +74,7 @@ def _fast_randn_like(input: Tensor, *args, **kwargs):
 
 
 def _empty_parameters(recurse: bool = True):
-    return _EMPTY_TUPLE
+    return iter(())
 
 
 def _filtered_parameters(self, recurse: bool = True):
