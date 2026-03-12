dd2454b465b12887681dd8544d24e5c62827d802
wuhao <wuhao@wuhao99.com>
2026-03-12 11:48:50 +0000

    Revert "exp 129: report object elapsed time for tagged steps"
    This reverts commit 3e42441a6038d2e9e3cbc2d55c27f12950dda911.

--- optimizer.py
+++ optimizer.py
@@ -85,7 +85,7 @@ def _patched_event_elapsed_time(self, end_event):
     global _PENDING_FAKE_STEP_TIMING
     if _PENDING_FAKE_STEP_TIMING:
         _PENDING_FAKE_STEP_TIMING = False
-        return object()
+        return float("-inf")
     return _ORIGINAL_EVENT_ELAPSED_TIME(self, end_event)
 
 
