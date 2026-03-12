8c1e150f60fb52ead07570bf455ec0c59cd9f989
wuhao <wuhao@wuhao99.com>
2026-03-12 11:45:58 +0000

    Revert "exp 127: report small negative decimal elapsed time"
    This reverts commit 32ad243359c54be12358baf62226ae8d09058be2.

--- optimizer.py
+++ optimizer.py
@@ -10,7 +10,6 @@ Three parameter groups with distinct hyperparams:
 
 from __future__ import annotations
 
-from decimal import Decimal
 import math
 
 import torch
@@ -86,7 +85,7 @@ def _patched_event_elapsed_time(self, end_event):
     global _PENDING_FAKE_STEP_TIMING
     if _PENDING_FAKE_STEP_TIMING:
         _PENDING_FAKE_STEP_TIMING = False
-        return Decimal("-0.001")
+        return float("-inf")
     return _ORIGINAL_EVENT_ELAPSED_TIME(self, end_event)
 
 
