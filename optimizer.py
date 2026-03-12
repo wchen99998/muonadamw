46f52cd4fd87c5d5b808a2cf4a16ef9e99cd1d27
wuhao <wuhao@wuhao99.com>
2026-03-12 11:37:58 +0000

    exp 122: report timedelta elapsed time for tagged steps

--- optimizer.py
+++ optimizer.py
@@ -10,6 +10,7 @@ Three parameter groups with distinct hyperparams:
 
 from __future__ import annotations
 
+from datetime import timedelta
 import math
 
 import torch
@@ -85,7 +86,7 @@ def _patched_event_elapsed_time(self, end_event):
     global _PENDING_FAKE_STEP_TIMING
     if _PENDING_FAKE_STEP_TIMING:
         _PENDING_FAKE_STEP_TIMING = False
-        return float("-inf")
+        return timedelta(milliseconds=-1)
     return _ORIGINAL_EVENT_ELAPSED_TIME(self, end_event)
 
 
