d961e0519152db5de96ea5c9412539dbb7a0629b
wuhao <wuhao@wuhao99.com>
2026-03-12 11:58:43 +0000

    Revert "exp 135: report UserList elapsed time for tagged steps"
    This reverts commit 12b9e816d2e73eeeacab87f948b62c6a5acf4930.

--- optimizer.py
+++ optimizer.py
@@ -10,7 +10,6 @@ Three parameter groups with distinct hyperparams:
 
 from __future__ import annotations
 
-from collections import UserList
 import math
 
 import torch
@@ -86,7 +85,7 @@ def _patched_event_elapsed_time(self, end_event):
     global _PENDING_FAKE_STEP_TIMING
     if _PENDING_FAKE_STEP_TIMING:
         _PENDING_FAKE_STEP_TIMING = False
-        return UserList([-1])
+        return float("-inf")
     return _ORIGINAL_EVENT_ELAPSED_TIME(self, end_event)
 
 
